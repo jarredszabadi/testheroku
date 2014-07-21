804c28cb2e416ea13315fb4ff75f07a358dffab6
Jarred - Personal <jarredszabadi@gmail.com>
2014-07-21 07:21:48 -0400

    removed ssh true

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, '/home/deploy/apps/testHeroku'
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-ssh_options[:forward_agent] = true
+
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
