ebcfdd6a1ede3ad4cf8a1e17c773e9c1c06f520b
Jarred - Personal <jarredszabadi@gmail.com>
2014-07-21 10:09:34 -0400

    messing with unicorn

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -6,7 +6,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deploy/apps/testHeorku/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deploy
+AS_USER=testHeorku
 set -u
 
 OLD_PIN="$PID.oldbin"
