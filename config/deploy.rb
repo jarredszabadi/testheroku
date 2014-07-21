119f5788f05b6624fbc7afccb98d6b102a49e1f6
Jarred - Personal <jarredszabadi@gmail.com>
2014-07-21 19:07:54 -0400

    chanegs to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'testHeroku'
+set :application, 'testheroku'
 set :repo_url, 'https://github.com/jarredszabadi/testheroku.git'
 set :deploy_to, '/home/deploy/apps/testHeroku'
 
@@ -37,7 +37,50 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+ set :keep_releases, 5
+
+
+
+#after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+# namespace :deploy do
+#   %w[start stop restart].each do |command|
+#     desc "#{command} unicorn server"
+#     task command, roles: :app, except: {no_release: true} do
+#       run "/etc/init.d/unicorn_#{application} #{command}"
+#     end
+#   end
+
+#   task :setup_config do
+#     on roles: :app do
+#       sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+#       sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+#       run "mkdir -p #{shared_path}/config"
+#       put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+#       puts "Now edit the config files in #{shared_path}."
+#     end
+#   end
+#   after "deploy:setup", "deploy:setup_config"
+
+#   task :symlink_config do
+#     on roles: :app do
+#       sudo "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+#     end
+#   end
+#   after "deploy:finalize_update", "deploy:symlink_config"
+
+#   desc "Make sure local git is in sync with remote."
+#   task :check_revision do
+#     on roles: :web do
+#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#         puts "WARNING: HEAD is not the same as origin/master"
+#         puts "Run `git push` to sync changes."
+#         exit
+#       end
+#     end
+#   end
+#   before "deploy", "deploy:check_revision"
+# end
 
 namespace :deploy do
 
@@ -54,9 +97,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-       #within release_path do
-        # execute :rake, 'cache:clear'
-       #end
+       within release_path do
+         execute :rake, 'cache:clear'
+       end
     end
   end
 
